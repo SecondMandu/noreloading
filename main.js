7d5dd0421828418a208e57da888ece9081e461be
SecondMandu <142383986+SecondMandu@users.noreply.github.com>
2023-08-27 18:08:06 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 UnitTypes.alpha.mineSpeed = 200
 UnitTypes.alpha.itemCapacity = 1000
 UnitTypes.alpha.health = 230
-Blocks.duo.reload = 0.1
-Blocks.arc.reload = 0.1
+Blocks.duo.reload = 0.5
+Blocks.arc.reload = 9
 Blocks.mechanicalDrill.drillTime = 100
-//Blocks.mechanicalDrill.mineTier = 4
 Blocks.conveyor.speed = 0.06
 Blocks.conveyor.health = 60
 Blocks.copperWall.health = 2000
